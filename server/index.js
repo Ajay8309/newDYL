959682731eb45b97147d9a1a1b17398681ff6b91
Ajay Panaskar <80744078+Ajay8309@users.noreply.github.com>
2026-02-21 14:26:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(helmet()); // Add security headers
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
 app.use(cors({
     origin: process.env.NODE_ENV === 'production'
         ? ['https://decodeyourlife.in', 'https://www.decodeyourlife.in']// Replace with actual production domain
